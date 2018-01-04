d9bdf86c116490819c62b7eae4a080e18b8fba78
Tamas Kuzdi <Tamas.Kuzdi@betssongroup.com>
2018-01-04 13:26:27 +0100

    filtering list

--- event-list.js
+++ event-list.js
@@ -1,6 +1,17 @@
 app.controller('eventSearch', [
     '$scope',
     function(scope){
-        scope.hello = 'szia';
+        scope.events = [
+            {
+                id: 1,
+                name: 'Manchester United - Chelsea FC',
+                time: new Date(2018,1,8,20,30,0)
+            },
+            {
+                id: 2,
+                name: 'Ferencvárosi TC - Újpest FC',
+                time: new Date(2018,2,1,15,0,0)
+            }
+        ];
     }
 ]);
