f081b6b743acf663bdd9e4567d6f844e20f2743c
Tamas Kuzdi <Tamas.Kuzdi@betssongroup.com>
2018-01-04 11:08:25 +0100

    add web server package and routes

--- app.js
+++ app.js
@@ -1,37 +1,24 @@
-(function(){
-    let app = angular.module('eventfinder', ['ui.router']);
+app.config([
+    '$stateProvider', '$urlRouterProvider',
+    function(stateProvider, urlRouterProvider){
+        urlRouterProvider.otherwise('/home');
 
-    app.controller('eventSearch', [
-        '$scope',
-        function(scope){
-            scope.hello = 'szia';
-        }
-    ]);
+        stateProvider            
+        .state('home', {
+            url: '/home',
+            templateUrl: 'home.html'
+        })
+        .state('home.list', {
+            url: '/list',
+            templateUrl: 'event-list.html',
+            controller: 'eventSearch'
+        })
+        .state('home.add', {
+            url: '/add',
+            templateUrl: 'add.html',
+            controller: function($scope){
 
-    app.config([
-        '$stateProvider', '$urlRouterProvider',
-        function(stateProvider, urlRouterProvider){
-            urlRouterProvider.otherwise('/home');
-
-            stateProvider            
-            .state('home', {
-                url: '/home',
-                templateUrl: 'home.html'
-            })
-            .state('home.list', {
-                url: '/list',
-                templateUrl: 'event-list.html',
-                controller: function($scope) {
-                    $scope.dogs = ['Bernese', 'Husky', 'Goldendoodle'];
-                }
-            })
-            .state('home.add', {
-                url: '/add',
-                templateUrl: 'add.html',
-                controller: function($scope){
-                    
-                }
-            });
-        }
-    ]);
-})();
+            }
+        });
+    }
+]);
